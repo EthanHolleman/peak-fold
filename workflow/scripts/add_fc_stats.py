1d0a0883bd9233958394c939c32bb181d0d1beb0
Ethan Thomas Holleman etholleman@ucdavis.edu <etholleman@ucdavis.edu>
2024-04-18 13:35:30 -0700

    add plasmid name column to output dataframe

--- workflow/scripts/add_fc_stats.py
+++ workflow/scripts/add_fc_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # STATS TO CALCULATE PER PEAK BASIS
 # - percent paired vs unpaired
@@ -25,6 +26,11 @@ def main():
     peak_df['converted'] = stat_df['converted']
     peak_df['unconverted'] = stat_df['unconverted']
     peak_df['paired'] = stat_df['paired']
+
+    peak_df['plasmid'] = peak_df.apply(
+        lambda row: extract_plasmid_name(row),
+        axis=1
+    )
 
     peak_df.to_csv(snakemake.output['out'], sep='\t', index=False)
 
@@ -59,17 +65,17 @@ def calc_percent_stats(row):
     """
     # Bisulfite converted vs unconverted basic stats
 
-    total_convert = [
+    total_convert = sum([
         1 for each_call in row.peak_call if each_call in CONVERTED
-        ]
-    total_unconverted = [
+        ])
+    total_unconverted = sum([
         1 for each_call in row.peak_call if each_call in UNCONVERTED
-        ]
+        ])
     
     total_convert_opps = total_convert + total_unconverted
 
     convert_percent = total_convert / total_convert_opps
-    unconvert_percent = total_unconverted / total_unconverted
+    unconvert_percent = total_unconverted / total_convert_opps
 
     # RNA folding paired vs unpaired basic stats
 
@@ -80,6 +86,20 @@ def calc_percent_stats(row):
         'unconverted': unconvert_percent,
         'paired': percent_paired
     }
+
+
+def extract_plasmid_name(row):
+    """Get the name of the plasmid from the sample description
+
+    Args:
+        row (Series): Peak dataframe row
+
+    Returns:
+        str: Name of the plasmid peak is called on
+    """
+
+    plasmid = re.search(r'plasmid(.+)desc', row['sample']).groups()[0]
+    return plasmid
 
 
 if __name__ == '__main__':
